423636c4f18976f014ca41209a539dbe2589509f
bennyharvey <johnphilips1973@gmail.com>
2022-08-07 19:28:01 +0300

    add new i18n messages

--- src/i18n/en-US/index.ts
+++ src/i18n/en-US/index.ts
@@ -1,8 +1,14 @@
 export default {
   failed: 'Action failed',
+  language: 'Language',
   success: 'Action was successful',
   greeting: 'Hi',
   app: {
     name: 'Homeostat UI',
+  },
+  name: 'Name',
+  join: 'Join',
+  warning: {
+    nameCantBeEmpty: 'Name can not be blank',
   },
 };
--- src/i18n/ru/index.ts
+++ src/i18n/ru/index.ts
@@ -1,8 +1,14 @@
 export default {
   failed: 'Не успешно',
+  language: 'Язык',
   success: 'Успешно',
   greeting: 'Привет',
   app: {
     name: 'Гомеостат',
+  },
+  name: 'Имя',
+  join: 'Начать',
+  warning: {
+    nameCantBeEmpty: 'Имя не может быть пустым',
   },
 };
